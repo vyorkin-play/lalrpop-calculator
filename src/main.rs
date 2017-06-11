9cd8d4da6c04e01e51aab3923293a42013a4aabe
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2017-06-11 20:44:21 +0300

    Add a precedence control example using "match"

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,16 @@ fn calculator2() {
     assert!(calculator2::parse_Term("((665)").is_err());
 }
 
+pub mod calculator2b;
+
+#[test]
+fn calculator2b() {
+    assert_eq!(calculator2b::parse_Term("11").unwrap(), "11");
+    assert_eq!(calculator2b::parse_Term("foo42").unwrap(), "id(foo42)");
+    assert_eq!(calculator2b::parse_Term("((22))").unwrap(), "twenty-two");
+    assert_eq!(calculator2b::parse_Term("(222)").unwrap(), "222");
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
